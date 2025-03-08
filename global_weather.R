02ff73f259200bc295d950ad52c9c93eed24af1d
herguad <g.s.herrera.s@gmail.com>
2025-03-08 16:25:14 -0300

    describe next steps in comments

--- global_weather.R
+++ global_weather.R
@@ -14,9 +14,16 @@ str(global_w)
 
 glimpse(global_w)
 
-#
 
 selected_gw <- global_w %>%
   select(country, location_name,last_updated,temperature_celsius,pressure_mb,precip_mm,wind_kph,humidity,uv_index,air_quality_Carbon_Monoxide,air_quality_Ozone,air_quality_Nitrogen_dioxide,air_quality_Sulphur_dioxide)
 
 str(selected_gw)
+
+#Check type uniformity and consistency. Manage inconsistencies.
+
+#Find and manage null values.
+
+#Find outliers and filter data appropriately.
+
+#Select  observations covering the same period of time.
