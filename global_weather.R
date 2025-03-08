688088d6c8b1c950608d66e6bcd171f309e19092
herguad <g.s.herrera.s@gmail.com>
2025-03-08 17:01:42 -0300

    comments on univariate summaries

--- global_weather.R
+++ global_weather.R
@@ -16,13 +16,16 @@ glimpse(global_w)
 
 
 selected_gw <- global_w %>%
-  select(country, location_name,last_updated,temperature_celsius,pressure_mb,precip_mm,wind_kph,humidity,uv_index,air_quality_Carbon_Monoxide,air_quality_Ozone,air_quality_Nitrogen_dioxide,air_quality_Sulphur_dioxide)
+  select(country, location_name,last_updated,temperature_celsius,pressure_mb,precip_mm,wind_kph,
+         humidity,uv_index,air_quality_Carbon_Monoxide,air_quality_Ozone,air_quality_Nitrogen_dioxide,air_quality_Sulphur_dioxide)
 
 str(selected_gw)
 
 #Check data uniformity and consistency. Manage inconsistencies.
 
-summary(selected_gw)
+summary(selected_gw) #<- pressure_mb, precip_mm, wind, and uv_index have remarkable max values 
+                     # while air_quality_CO2 and air_quality_SO2 have unusual min values
+                     # min uv_index is zero which means no sunlight and should be looked into
 
 #Find outliers and manage data appropriately.
 
