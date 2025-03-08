e428270d4086336b7ef8ebc58eaca16ea225511e
herguad <g.s.herrera.s@gmail.com>
2025-03-08 16:20:47 -0300

    set_questions and select variables

--- global_weather.R
+++ global_weather.R
@@ -5,8 +5,18 @@ setwd("C:/Users/Guadalupe/Documents/IT/R/weather_data")
 
 global_w <- read.csv("GlobalWeatherRepository.csv",header=TRUE, sep = ",", fill = TRUE)
 
+## Which locations display similar weather trends according to the observations in this db?
+## Which locations display greater weather changes in the same period of time?
+
 #View(global_w)
 
 str(global_w)
 
+glimpse(global_w)
 
+#
+
+selected_gw <- global_w %>%
+  select(country, location_name,last_updated,temperature_celsius,pressure_mb,precip_mm,wind_kph,humidity,uv_index,air_quality_Carbon_Monoxide,air_quality_Ozone,air_quality_Nitrogen_dioxide,air_quality_Sulphur_dioxide)
+
+str(selected_gw)
